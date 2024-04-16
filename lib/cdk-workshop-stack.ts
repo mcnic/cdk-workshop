635fb368e369a0a53656c3aa8b47e9e9f98b2f3c
max <mcnic@users.noreply.github.com>
2024-04-16 15:01:43 -0700

    eat: add Lambda & API gatawey

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,20 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
+    });
   }
 }
