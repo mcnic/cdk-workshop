4ebfaf1a338226cf02942ca41a02b4273d513580
max <mcnic@users.noreply.github.com>
2024-04-20 20:48:12 -0700

    feat: add amd for pipeline

--- src/pipeline-stack.ts
+++ src/pipeline-stack.ts
@@ -1,13 +1,28 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Repository } from 'aws-cdk-lib/aws-codecommit';
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class WorkshopPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Repository(this, 'WorkshopRepo', {
+    const repo = new Repository(this, 'WorkshopRepo', {
       repositoryName: 'WorkshopRepo',
+    });
+
+    const synth = new CodeBuildStep('SynthStep', {
+      input: CodePipelineSource.codeCommit(repo, 'main'),
+      commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      installCommands: ['npm install -g aws-cdk'],
+    });
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      synth,
     });
   }
 }
